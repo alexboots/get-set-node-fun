1c09b22f0f973e69ff1e38247826900af6350768
Alex Hays <alexboots@Alexs-MacBook-Pro.local>
2017-07-18 13:23:02 -0600

    add error

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const http = require('http');
 
 const server = http.createServer().listen(4000, '127.0.0.1');
 
+
+let body = []
 server.on('request', (request, response) => {
   const { method, url, headers } = request;
   const userAgent = headers['user-agent'];
+
+  request
+    .on('error', (error) => {
+      console.log('Oh no! Something went wrong!\n');
+      console.error(error.stack)
+    })
+    .on('data', (chunk) => {
+      body.push(chunk);
+      console.log("chunk", chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+      console.log('body', body);
+    })
   
 });
