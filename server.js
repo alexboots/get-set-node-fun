f6cf2aec9ba3a76d27001d022f4dbc34bee9800a
Alex Hays <alexboots@Alexs-MacBook-Pro.local>
2017-07-18 15:58:07 -0600

    learn regex

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
+/*
+
+Database server
+
+[✔] Before your interview, write a program that runs a server that is accessible on http://localhost:4000/. 
+[] When your server receives a request on http://localhost:4000/set?somekey=somevalue it should store the passed key and value in memory. 
+[] When it receives a request on http://localhost:4000/get?key=somekey it should return the value stored at somekey.
+
+During your interview, you will pair on saving the data to a file. 
+You can start with simply appending each write to the file, and work on making it more efficient if you have time.
+
+*/
+
 const http = require('http');
 const server = http.createServer().listen(4000, '127.0.0.1');
 
+// Constants
+const GET = 'GET'
 const STATUS_CODE_200 = 200
 const STATUS_CODE_404 = 404
-const GET = 'GET'
 
 
 server.on('request', (request, response) => {
 
-  const { method, url, headers } = request;
-  const userAgent = headers['user-agent'];
+  const { url } = request;
 
-
-  if(request.method === GET && request.url === '/hello') {
-    
+  url
+  
+  set?somekey=somevalue
+  if() {
     let body = []
     request
       .on('error', (error) => {
@@ -24,13 +38,19 @@ server.on('request', (request, response) => {
       })
       .on('end', () => {
         body = Buffer.concat(body).toString();
+
+
+
+        console.log('url', url.);
         
         response.on('error', (error) => { console.error('Error responding to your requesty', error); })
 
         response.statusCode = STATUS_CODE_200;
         response.setHeader('Content-Type', 'application/json');
 
-        const responseBody = { headers, method, url, body };
+        const responseBody = {  url, body };
+
+        // Stuff
         response.write(JSON.stringify(responseBody));
         response.end();
       })
