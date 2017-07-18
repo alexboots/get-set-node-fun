88be79927c04cea5b4ae4bce86d00d81e4c77055
Alex Hays <alexboots@Alexs-MacBook-Pro.local>
2017-07-18 13:32:54 -0600

    response with stuff

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const http = require('http');
-
 const server = http.createServer().listen(4000, '127.0.0.1');
 
+const STATUS_CODE_200 = 200
+const STATUS_CODE_404 = 404
 
-let body = []
+
 server.on('request', (request, response) => {
+
   const { method, url, headers } = request;
   const userAgent = headers['user-agent'];
-
+  
+  let body = []
   request
     .on('error', (error) => {
       console.log('Oh no! Something went wrong!\n');
@@ -15,11 +18,17 @@ server.on('request', (request, response) => {
     })
     .on('data', (chunk) => {
       body.push(chunk);
-      console.log("chunk", chunk);
     })
     .on('end', () => {
       body = Buffer.concat(body).toString();
-      console.log('body', body);
+      
+      response.on('error', (error) => { console.error('Error responding to your requesty', error); })
+
+      response.statusCode = STATUS_CODE_200;
+      response.setHeader('Content-Type', 'application/json');
+
+      const responseBody = { headers, method, url, body };
+      response.write(JSON.stringify(responseBody));
+      response.end();
     })
-  
 });
